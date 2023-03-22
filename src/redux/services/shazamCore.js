090862b6ba3417f83bd75bcb7c44f12e15c7d0b5
Prashant Naik <bhimanprash16@gmail.com>
2023-03-22 21:11:26 +0530

    commited

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
+
     export const shazamCoreApi = createApi({
         reducerPath: 'shazamCoreApi',
         baseQuery: fetchBaseQuery({
